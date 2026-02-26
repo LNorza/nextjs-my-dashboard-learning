ebbf666d0496981dcf0949e275246a696cf31f0f
LNorza <luiscnorza@gmail.com>
2026-02-25 19:51:28 -0700

    feature(counter) : add counter page

--- app/dashboard/counter/page.tsx
+++ app/dashboard/counter/page.tsx
@@ -1,7 +1,15 @@
+import { CartCounter } from '@/app/shopping-cart';
+
+const metadata = {
+  title: 'Counter Page',
+  description: 'A simple counter page to demonstrate state management in Next.js',
+};
+
 const CounterPage = () => {
   return (
-    <section>
-      <h1>Counter Page</h1>
+    <section className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <CartCounter value={0} />
     </section>
   );
 };
